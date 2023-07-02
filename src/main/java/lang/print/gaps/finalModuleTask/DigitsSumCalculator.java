691d2cc7926946f2da0b0f862a13ee1e9227030f
NeverRIEght <lfkomarovme@gmail.com>
2023-07-03 00:44:11 +0300

    Digits sum calculator

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int thousands = number / 1000;
+        int hundreds = number % 1000 / 100;
+        int tens = number % 100;
+        int numbers = tens % 10;
+        tens /= 10;
+
+        System.out.println(thousands + hundreds + tens + numbers);
     }
 }
